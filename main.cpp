04249c652d6086d587f39a1efafe57bd40331ab0
Thomas Le Magoariec <123191003+ThomasLeMago@users.noreply.github.com>
2023-11-08 19:04:18 +0100

    added memory functions

--- main.cpp
+++ main.cpp
@@ -9,13 +9,19 @@ using u32 = unsigned int;
 struct Mem
 {
     static constexpr u32 MAX_MEM = 1024 * 64;
-    byte Data[MAX_MEM];
+    byte data[MAX_MEM];
 
     void init()
     {
         for (u32 i = 0; i < MAX_MEM; i++){
-            Data[i] = 0;
+            data[i] = 0;
         }
+    }
+
+    // read byte
+    byte operator[](u32 addr) const
+    {
+        return data[addr];
     }
 };
 
@@ -45,6 +51,23 @@ struct CPU
         memory.init();
     }
 
+    byte fetchByte(u32& cycles, Mem& memory)
+    {
+        byte data = memory[PC];
+        PC++;
+        cycles--;
+
+        return data;
+    }
+
+    void execute(u32 cycles, Mem& memory)
+    {
+        while (cycles > 0)
+        {
+            byte ins = fetchByte(cycles, memory);
+        }
+    }
+
 };
 
 int main()
@@ -52,5 +75,6 @@ int main()
     Mem mem;
     CPU cpu;
     cpu.reset(mem);
+    cpu.execute(2, mem);
     return 0;
 }
